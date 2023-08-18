99ca51917244ae714066bff518c9da7b50edc8c6
Julio Faerman <jufaerma@redhat.com>
2023-08-18 08:58:09 +0000

    wip

---
+++ publish-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+
+./mvnw package -Dnative -DskipTests
+docker build -f src/main/docker/Dockerfile.native-micro -t caravanacloud/rinhadebackend .
+echo 'docker run -i --rm -p 9090:9090 caravanacloud/rinhadebackend'
